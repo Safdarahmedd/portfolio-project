c32f8c273a051c256786495aaa7360f690664adf
Safdar Ahmed <safdarahmed@Safdars-MacBook-Air.local>
2020-01-21 17:31:35 +0530

    added jobs and bootstrap functionality

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=128)
+    image = models.ImageField(upload_to='images/')
+    body  = models.TextField()
+    date_pub = models.DateField(auto_now=False, auto_now_add=True)
