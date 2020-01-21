b1f552ca55fab0795263d8f3482d40d36dce6d62
Safdar Ahmed <safdarahmed@Safdars-MacBook-Air.local>
2020-01-21 23:17:38 +0530

    final touches

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,9 @@ class Blog(models.Model):
     image = models.ImageField(upload_to='images/')
     body  = models.TextField()
     date_pub = models.DateField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
